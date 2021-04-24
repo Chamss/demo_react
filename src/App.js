caa7da9fbce4d9fc030a9dd7ff4f161a756344b3
chams <chamseddinechelly@gmail.com>
2021-04-24 11:32:20 +0100

    Handling arrays

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
 import { useState } from 'react';
 
 const App = () => {
-  // const [left, setLeft] = useState(0)
-  // const [right, setRight] = useState(0)
-  const [clicks, setClicks] = useState({ left: 0, right: 0 })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  // const handleLeftClick = () => {
-  //   const newClicks = {
-  //     // right: clicks.right,
-  //     ...clicks,
-  //     left: clicks.left + 1
-  //   }
-  //   setClicks(newClicks)
-  // }
-  const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
-  // const handleLeftClick = () =>{
-  //   clicks.left++
-  //   setClicks(clicks)
-  // }
-  const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
-  // const handleRightClick = () => {
-  //   const newClicks = {
-  //     // left: clicks.left,
-  //     ...clicks,
-  //     right: clicks.right + 1
-  //   }
-  //   setClicks(newClicks)
-  // }
-
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+  // const handleLeftClick = () => setClicks({ ...clicks, left: clicks.left + 1 })
+  // const handleRightClick = () => setClicks({ ...clicks, right: clicks.right + 1 })
   return (
-    <div className="text-center">
-      {clicks.left}
-      <button onClick={handleLeftClick} className="btn btn-info">
-        Left
-        </button>
-      <button onClick={handleRightClick} className="btn btn-danger">
-        Right
-        </button>
-      {clicks.right}
+    <div className="row">
+      <div className="col-sm-6 col-sm-offset-3 text-center margTop30">
+        <b>{left}</b>
+        <div className="btn-group">
+          <button onClick={handleLeftClick} className="btn btn-info">
+            Left
+          </button>
+          <button onClick={handleRightClick} className="btn btn-danger">
+            Right
+          </button>
+        </div>
+        <b>{right}</b>
+        <hr />
+        <h3>{allClicks.join(' ')}</h3>
+      </div>
     </div>
   )
 }
